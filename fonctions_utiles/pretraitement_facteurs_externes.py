e04b022685127f86a2746d6151d63c7800689bfb
amaury_triomphe <163183066+t-amaury@users.noreply.github.com>
2024-08-13 09:43:48 +0200

    Add files via upload

---
+++ fonctions_utiles/pretraitement_facteurs_externes.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+
+def reindex_exogenes(start_forecast, end_forecast, facteur_externe):
+    date_rng2 = pd.date_range(start=start_forecast, end=end_forecast, freq='D')
+    weekly_exog2 = facteur_externe.resample('D').ffill().reindex(date_rng2).fillna(method='ffill')
+    return(weekly_exog2)
+
+def transform_covid_exo(weekly_exog2):
+    weekly_exog2['covid'] = 0
+    return(weekly_exog2)
+
+def filtrage_pays(weekly_exog2, pays_A, pays_B):
+    weekly_exog2 = weekly_exog2.loc[:, [pays_A, pays_B, "covid"]]
+    return weekly_exog2
+
+def reformatage(weekly_exog2):
+    # Reformater les données pour StatsForecast
+    test_exog = weekly_exog2.reset_index()
+    test_exog.columns = ['ds', 'GDP_A', 'GGE_A', 'population_A', 'GDP_B', 'GGE_B', 'population_B', 'covid']
+    test_exog['unique_id'] = 1  # Ajouter une colonne 'unique_id' avec une valeur constante
+    test_exog = test_exog[['unique_id', 'ds', 'GDP_A', 'GGE_A', 'population_A', 'GDP_B', 'GGE_B', 'population_B', 'covid']]  # Réordonner les colonnes
+    return(test_exog)
+    
+def pretraitement2(start_forecast, end_forecast, facteur_externe, pays_A, pays_B):
+    facteur_externe = facteur_externe.set_index(["ICAO_Code", "Subject Descriptor"]).transpose().loc['2017':'2029']
+    facteur_externe.index= pd.to_datetime(facteur_externe.index, format='%Y')
+    weekly_exog2 = reindex_exogenes(start_forecast, end_forecast, facteur_externe)
+    weekly_exog2 = transform_covid_exo(weekly_exog2)
+    weekly_exog2 = filtrage_pays(weekly_exog2, pays_A, pays_B)
+    test_exog = reformatage(weekly_exog2)
+    return test_exog
---
+++ fonctions_utiles/update_predictions.py
@@ -0,0 +1,65 @@
+import numpy as np
+np.float_ = np.float64
+import pandas as pd
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from prophet import Prophet
+import os
+import joblib
+from fonctions_utiles.pretraitement_facteurs_externes import pretraitement2
+
+def load_model(region_A, region_B):
+    """Load the prediction models for a given pair of regions."""
+    prophet_model_path = f'model/prophet_model_{region_A}_{region_B}.joblib'
+    exp_smoothing_model_path = f'model/exp_smoothing_model_{region_A}_{region_B}.joblib'
+    
+    model = {}
+    if os.path.exists(prophet_model_path):
+        model['prophet'] = joblib.load(prophet_model_path)
+    if os.path.exists(exp_smoothing_model_path):
+        model['exp_smoothing'] = joblib.load(exp_smoothing_model_path)
+    return model
+
+def regroupement_annees2(forecasts_dict):
+    rows = []
+    for key, series in forecasts_dict.items():
+        # Convertir la série en DataFrame
+        df = series.to_frame(name='value')
+        # Ajouter la colonne pour la paire
+        df['pair'] = '_'.join(key)
+        # Ajouter la colonne pour l'année
+        df['year'] = df.index.year
+        rows.append(df)
+
+    combined_df = pd.concat(rows)
+    grouped = combined_df.groupby(['pair', 'year'])['value'].sum().reset_index()
+    pivot_df = grouped.pivot_table(index='pair', columns='year', values='value', fill_value=0)
+    pivot_df.reset_index(inplace=True)
+    return pivot_df
+    
+def update_predictions(facteur_externe_df, facteur_externe_petrole_df, prevision_df, start_date, end_date):
+    """Update predictions based on modified external factors."""
+    # Placeholder for recalculating predictions
+    forecasts_dict = {}
+
+    # Get all unique region pairs
+    region_pairs = prevision_df['pair'].unique()
+    
+    for pair in region_pairs:
+        region_A, region_B = pair.split('--')
+        model = load_model(region_A, region_B)
+        test_exog = pretraitement2(start_date, end_date, facteur_externe_df, region_A, region_B)
+
+        if 'prophet' in model:
+            forecast_prophet = model.predict(test_exog)
+            forecast = pd.Series(list(forecast_prophet["yhat"]), index=list(forecast_prophet["ds"]))
+        elif "exp_smoothing" in model:
+            results = model.fit()
+            params = model.params
+            forecast = model.predict(start = start_date, end = end_date, params=params)
+            forecast = pd.Series(forecast, index=date_range)
+        else:
+            forecast = [0.147] * (len(date_range))
+            forecast = pd.Series(forecast, index=date_range)
+        forecasts_dict[(region_A, region_B)] = forecast
+        predictions = regroupement_annees2(forecasts_dict)
+    return predictions
